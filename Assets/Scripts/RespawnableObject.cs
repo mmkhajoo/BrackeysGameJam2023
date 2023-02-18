9b2d2fd286c9a9453be721bba66ede775d9b7302
mmkhajoo <mmkhajoo@gmail.com>
2023-02-19 03:13:28 +0330

    Respawnable Object Added.

---
+++ Assets/Scripts/RespawnableObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Managers
+{
+    public class RespawnableObject : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _onRespawn;
+
+        [SerializeField] private float _respawnDelay;
+
+        private Vector3 _initialPosition;
+
+        private void Start()
+        {
+            _initialPosition = transform.position;
+        }
+
+        private async void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Water"))
+            {
+                gameObject.SetActive(false);
+
+                await Respawn();
+                
+                _onRespawn?.Invoke();
+            }
+        }
+
+        private async Task Respawn()
+        {
+            await Task.Delay((int)(_respawnDelay*1000));
+
+            transform.position = _initialPosition;
+            gameObject.SetActive(true);
+            
+        }
+    }
+}
